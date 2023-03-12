65ff5e5c3fe87982692a35113acef54a72658904
Patricio <patoestradac@gmail.com>
2023-03-11 23:10:18 -0500

    Modelo de usuarios actualizado para fines de la tarea 6

--- DisenioWebTema7/database/migrations/2023_03_12_001104_create_usuarios_table.php
+++ DisenioWebTema7/database/migrations/2023_03_12_001104_create_usuarios_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
+            $table->string('NombreUsuario')->primary();
+            $table->string('Correo');
+            $table->string('Password');
             $table->string('Nombre');
             $table->string('ApellidoPaterno');
             $table->string('ApellidoMaterno');
