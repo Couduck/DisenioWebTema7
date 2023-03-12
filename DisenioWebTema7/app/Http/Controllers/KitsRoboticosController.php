3d290c3928916fed39211f379908c68bbcff79c4
Patricio <patoestradac@gmail.com>
2023-03-11 20:55:53 -0500

    Migraciones creadas y cargadas

---
+++ DisenioWebTema7/app/Http/Controllers/KitsRoboticosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KitsRoboticos;
+use Illuminate\Http\Request;
+
+class KitsRoboticosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KitsRoboticos $kitsRoboticos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KitsRoboticos $kitsRoboticos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KitsRoboticos $kitsRoboticos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KitsRoboticos $kitsRoboticos)
+    {
+        //
+    }
+}
